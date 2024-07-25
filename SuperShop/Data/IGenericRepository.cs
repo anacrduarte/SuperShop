10afa34e7df32a07256140231507be9ddf1985d3
anacrduarte <ana_cris_duarte@hotmail.com>
2024-07-25 09:35:29 +0100

    Generic repository

---
+++ SuperShop/Data/IGenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperShop.Data
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        /// <summary>
+        /// Devolve todas as entidades
+        /// </summary>
+        /// <returns></returns>
+        IQueryable<T> GetAll();
+
+        /// <summary>
+        /// Devolve por id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<T> GetByIdAsync(int id);
+
+        /// <summary>
+        /// Criar entidade do tipo T(genérico)
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task CreateAsync(T entity);
+
+        /// <summary>
+        /// Update entidade do tipo T
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task UpdateAsync(T entity);
+
+        /// <summary>
+        /// Delete entity type T
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task DeleteAsync(T entity);
+
+        /// <summary>
+        /// see if exists
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<bool> ExistAsync(int id);
+    }
+}
