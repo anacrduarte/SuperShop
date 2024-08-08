324101a2f2eb6adc7e3231726daaf9fd9b20779d
anacrduarte <ana_cris_duarte@hotmail.com>
2024-08-08 17:40:38 +0100

    Order models

---
+++ SuperShop/Data/IOrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using SuperShop.Data.Entities;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperShop.Data
+{
+    public interface IOrderRepository: IGenericRepository<Order>
+    {
+        /// <summary>
+        /// all of the orders of that user
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns>table of orders</returns>
+        Task<IQueryable<Order>> GetOrderAsync(string userName);
+    }
+}
--- SuperShop/Startup.cs
+++ SuperShop/Startup.cs
@@ -45,6 +45,7 @@ namespace SuperShop
             services.AddScoped<IBlobHelper, BlobHelper>();
             services.AddScoped<IConverterHelper, ConverterHelper>();
             services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
             services.ConfigureApplicationCookie(options =>
             {
                 options.LoginPath = "/Account/NotAuthorized";
